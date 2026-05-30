d978007717fbd8a5df52b131a5f8e2326849252c
Ivanov-Georgii <gevyivanov@edu.hse.ru>
2026-05-30 13:31:21 +0300

    Сделана корректная реализация обработки тела функции

--- src/FileReader.py
+++ src/FileReader.py
@@ -17,21 +17,32 @@ class FileReader:
                 iterSubject = False
                 iterAdress = False
                 importantInfo = [[],[],[]]
+                bodyStart = -1
+                count = 0
                 body = ""
                 for line in text.split("\n"):
                     if not line:
                         continue
                     else:
-                        if iterSubject and iterAdress:
-                            body += line
-                        if ((line.find("Subject: "))==0 or (line.find("Тема: "))==0) and iterSubject == False:
+                        count+=1
+                        if ((line.find("Subject:"))==0 or (line.find("Тема:"))==0) and iterSubject == False:
                             importantInfo[0]=(("Subject", line[line.find(":")+1:]))
                             firstIterSub = line
+                            bodyStart = count
                             iterSubject = True
                         if ((line.find("From: "))==0 or (line.find("От кого: "))==0) and iterAdress == False:
                             importantInfo[1]=("From", line[line.find(":")+1:])
                             firstIterAdress = line
+                            bodyStart = count
                             iterAdress = True
+                for line in text.split("\n"):
+                    if not line:
+                        continue
+                    else:
+                        bodyStart -= 1
+                        if bodyStart<0:
+                            body+=line+" "
+                importantInfo[2]=("Body",body)
                 if not iterAdress:
                     print("отправить файл в битое")
                 if body:
