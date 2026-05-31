c8b5e27d694d1f2ef3b913d5b17c27d55dc0b920
Anastasia2807 <Kirillovaan07@yandex.ru>
2026-05-31 11:56:37 +0300

    добавление логов в класс FileReader

--- src/FileReader.py
+++ src/FileReader.py
@@ -6,6 +6,7 @@ class FileReader:
     def __init__(self, folder):
         self.folder = Path(folder)
         self.unreadable = Path("../SortedInbox/Unreadable")
+        logging.info(f"FileReader готов к работе. Папка: {self.folder}")
     def read_files(self):
         data = []
         for file in self.folder.iterdir():
@@ -38,8 +39,10 @@ class FileReader:
                             iterAdress = True
                 if not iterSubject:
                     importantInfo[0] = ("Subject", "")
+                    logging.info(f"В файле {file.name} не найдена тема")
                 if not iterAdress:
                     importantInfo[1] = ("From", "")
+                    logging.info(f"В файле {file.name} не найден отправитель")
                 for line in text.split("\n"):
                     if not line:
                         continue
@@ -51,13 +54,16 @@ class FileReader:
                 importantInfo[3] = ("Name",file.name)
                 if not iterAdress:
                     shutil.move(str(file), str(self.unreadable / file.name))
+                    logging.info(f"Файл {file.name} отправлен в категорию 'несортируемые'")
                     print("Файл " + file.name + " отправлен в несортируемые")
                     continue
                 data.append(importantInfo)
             except:
+                logging.error(f"Файл {file.name} не удалось прочитать, поэтому он отправлен в категорию 'несортируемые'")
                 print("Файл " + file.name + " отправлен в несортируемые")
                 try:
                     file.rename(self.unreadable / file.name)
+
                 except:
                     pass
         return data
