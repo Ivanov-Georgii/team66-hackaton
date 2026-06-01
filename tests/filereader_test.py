37ede50700d6c6411270cda3b5dc5af1b7bdd5ec
lucentblessed <ivan.shatalov04@gmail.com>
2026-06-02 00:25:37 +0300

    исправил тесты для класса FileReader

--- tests/filereader_test.py
+++ tests/filereader_test.py
@@ -60,8 +60,35 @@ def test5():
     assert found
 
 
+def test6():
+    frd = FileReader('../TestFolder')
+    frd.unreadable = Path("../SortedInbox/Unreadable")
+    frd.read_files()
+    moved_file = Path("../SortedInbox/Unreadable/mail6.txt")
+    assert moved_file.exists()
+
+def test7():
+    frd = FileReader('../TestFolder')
+    frd.unreadable = Path("../SortedInbox/Unreadable")
+    frd.read_files()
+    moved_file = Path("../SortedInbox/Unreadable/mail7.bin")
+    assert moved_file.exists()
+
+
+def test8():
+    frd = FileReader('../TestFolder')
+    frd.unreadable = Path("../SortedInbox/Unreadable")
+    result = frd.read_files()
+    found = False
+    for item in result:
+        if item[3][1] == "mail8.txt":
+            assert item[1] == ("From", "a.fedorova@company.ru")
+            assert item[0] == ("Subject", "izmenenie grafika raboty")
+            found = True
+    assert found
+
+
 def test_kostil():
-    src = Path("../SortedInbox/Unreadable/mail2.txt")
-    dst = Path("../TestFolder/mail2.txt")
-    if src.exists():
-        src.replace(dst)
+    shutil.move("../SortedInbox/Unreadable/mail2.txt", "../TestFolder/mail2.txt")
+    shutil.move("../SortedInbox/Unreadable/mail6.txt", "../TestFolder/mail6.txt")
+    shutil.move("../SortedInbox/Unreadable/mail7.bin", "../TestFolder/mail7.bin")
