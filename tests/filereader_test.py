10a6cf0080299a1105351ec8c31f29ab2b1a53be
veylsergey2008-ux <veyl.sergey.2008@gmail.com>
2026-06-01 18:15:51 +0300

    Update by serj filereader_test.py

--- tests/filereader_test.py
+++ tests/filereader_test.py
@@ -17,3 +17,35 @@ def test2():
     frd.read_files()
     moved_file = Path("TestFolder/Unreadable/mail2.txt")
     assert moved_file.exists() == False
+
+def test3():
+    frd.unreadable = Path("TestFolder/Unreadable")
+    result = frd.read_files()
+    found = False
+    for item in result:
+        if item[3][1] == "mail3.txt":
+            assert "text message" in item[2][1]
+            found = True
+    assert found
+
+
+def test4():
+    frd.unreadable = Path("TestFolder/Unreadable")
+    result = frd.read_files()
+    found = False
+    for item in result:
+        if item[3][1] == "mail4.txt":
+            assert item[1] == ("From", "user@mail.com")
+            found = True
+    assert found
+
+
+def test5():
+    frd.unreadable = Path("TestFolder/Unreadable")
+    result = frd.read_files()
+    found = False
+    for item in result:
+        if item[3][1] == "mail5.txt":
+            assert item[0] == ("Subject", "")
+            found = True
+    assert found
